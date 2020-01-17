455c54041000a8c5d4f232c8ebb9f635eb11bd0b
nathandfe <nathandfe@outlook.com>
2020-01-17 16:52:43 +0000

    Story3 - Added code to make Alternative Fuel Tests work.

--- exercises/dotnet/TaxCalculator/DefaultTaxCalculator.cs
+++ exercises/dotnet/TaxCalculator/DefaultTaxCalculator.cs
@@ -5,6 +5,27 @@
         public int CalculateTax(Vehicle vehicle)
         {
             int tax = 0;
+            switch (vehicle.FuelType)
+            {
+                case FuelType.Petrol:
+                    tax = PetrolTax(vehicle);
+                    break;
+                case FuelType.Diesel:
+                    break;
+                case FuelType.Electric:
+                    break;
+                case FuelType.AlternativeFuel:
+                    tax = AlternativeFuelTax(vehicle);
+                    break;
+                default:
+                    break;
+            }
+            return tax;
+        }
+
+        private static int PetrolTax(Vehicle vehicle)
+        {
+            int tax = 0;
             if (vehicle != null)
             {
                 if (vehicle.Co2Emissions == 0)
@@ -60,6 +81,69 @@
                     tax = 2070;
                 }
             }
+
+            return tax;
+        }
+
+        private static int AlternativeFuelTax(Vehicle vehicle)
+        {
+            int tax = 0;
+            if (vehicle != null)
+            {
+                if (vehicle.Co2Emissions == 0)
+                {
+                    tax = 0;
+                }
+                else if (vehicle.Co2Emissions >= 1 && vehicle.Co2Emissions <= 50)
+                {
+                    tax = 0;
+                }
+                else if (vehicle.Co2Emissions >= 51 && vehicle.Co2Emissions <= 75)
+                {
+                    tax = 15;
+                }
+                else if (vehicle.Co2Emissions >= 76 && vehicle.Co2Emissions <= 90)
+                {
+                    tax = 95;
+                }
+                else if (vehicle.Co2Emissions >= 91 && vehicle.Co2Emissions <= 100)
+                {
+                    tax = 115;
+                }
+                else if (vehicle.Co2Emissions >= 101 && vehicle.Co2Emissions <= 110)
+                {
+                    tax = 135;
+                }
+                else if (vehicle.Co2Emissions >= 111 && vehicle.Co2Emissions <= 130)
+                {
+                    tax = 155;
+                }
+                else if (vehicle.Co2Emissions >= 131 && vehicle.Co2Emissions <= 150)
+                {
+                    tax = 195;
+                }
+                else if (vehicle.Co2Emissions >= 151 && vehicle.Co2Emissions <= 170)
+                {
+                    tax = 505;
+                }
+                else if (vehicle.Co2Emissions >= 171 && vehicle.Co2Emissions <= 190)
+                {
+                    tax = 820;
+                }
+                else if (vehicle.Co2Emissions >= 191 && vehicle.Co2Emissions <= 225)
+                {
+                    tax = 1230;
+                }
+                else if (vehicle.Co2Emissions >= 226 && vehicle.Co2Emissions <= 255)
+                {
+                    tax = 1750;
+                }
+                else if (vehicle.Co2Emissions > 255)
+                {
+                    tax = 2060;
+                }
+            }
+
             return tax;
         }
     }
